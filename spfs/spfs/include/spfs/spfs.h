a9f4e4a1521ecceeef59c6241694def57b8a4968
JeppeSRC <jeppe97rc@gmail.com>
2017-05-17 20:49:41 +0200

    spfs format defined

---
+++ spfs/spfs/include/spfs/spfs.h
@@ -0,0 +1,37 @@
+#pragma once
+
+typedef unsigned char	byte;
+typedef unsigned short	word;
+typedef unsigned int	dword;
+
+typedef struct _SPFS_HEADER {
+	byte  Reserved0[3];
+	word  Version;
+	word  Reserved1;
+	byte  VolumeLabel[16];
+	word  BytesPerSecond;
+	byte  ReservedSectors;
+	dword NumSectorsLow;
+	dword NumSectorsHigh;
+	dword FileTables;
+} SPFS_HEADER;
+
+typedef struct _SPFS_DATE_TIME {
+	byte minutes : 6;
+	byte hours : 5;
+	byte dayOfMonth : 5;
+	word year : 12;
+	byte month : 4;
+} SPFS_DATE_TIME;
+
+typedef struct _SPFS_FILE_ENTRY {
+	byte			Type;
+	byte			Reserved0;
+	word			Reserved1;
+	byte			Name[40];
+	SPFS_DATE_TIME	CreationDate;
+	dword			SizeLow;
+	dword			SizeHigh;
+	dword			LBALow;
+	dword			LBAHigh;
+} SPFS_FILE_ENTRY;
