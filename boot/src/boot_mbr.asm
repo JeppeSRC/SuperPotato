3c627b7fa142acb90c642133005826d5afbbe7e0
JeppeSRC <jeppe97rc@gmail.com>
2017-08-19 02:26:30 +0200

    idk

--- boot/src/boot_mbr.asm
+++ boot/src/boot_mbr.asm
@@ -6,15 +6,28 @@ jmp start
 
 drive_number: db 0
 
-drive:
-drive_size: db 0x10
-drive_zero: db 0
-drive_numSectors: dw 0
-drive_segment: dw 0x0
-drive_offset: dw 0x0
-drive_lba_low: dd 0
-drive_lba_high: dd 0
+sfs_ID: 				dw 0x00
+sfs_Version: 			dw 0x00
+sfs_Reserved:			dw 0x00
+sfs_Label:				times 16 db 0x00
+sfs_ReservedSectors:	db 0x00
+sfs_SectorSize:			dw 0x00
+sfs_ClusterSize:		db 0x00
+sfs_TrackingSectors:	dq 0x00
+sfs_DatagSectors:		dq 0x00
 
+drive:
+drive_dap_size: 		db 0x10
+drive_zero:				db 0x00
+drive_num:				dw 0x00
+drive_segment:			dw 0x00
+drive_offset:			dw 0x00
+drive_LBALow:			dd 0x00
+drive_LBAHigh:			dd 0x00
+
+%define SFS_ENTRY_SIZE 34
+
+%if 0
 print:
 	push ax
 	mov ah, 0x0E
@@ -28,7 +41,7 @@ print:
 	.done:
 	pop ax
 	ret
-
+%endif
 
 start:
 	BREAK
@@ -40,11 +53,25 @@ start:
 
 	mov byte [drive_number], dl
 
+	xor ax, ax
+	mov ax, word [sfs_SectorSize]
+	mul byte [sfs_ClusterSize]
+
+	mov dl, SFS_ENTRY_SIZE
+	div dl
+
+	xor eax, eax
+	mov al, byte [sfs_ReservedSectors]
+	add eax, dword [sfs_TrackingSectors]
+
+
+
 	hlt
-	
+
 readSectors:
 	push ax
 	push dx
+	mov si, drive
 	mov ah, 0x42
 	mov dl, byte [drive_number]
 	int 0x13
@@ -55,13 +82,9 @@ readSectors:
 	ret
 
 	.readSectors_error:
-	mov si, readFail
-	call print
-	pop dx
-	pop ax
-	ret
+		hlt
 
-readFail: db 'Failed to read sectors', 0
+filename: db 'boot/boot_mbr2.bin'
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
